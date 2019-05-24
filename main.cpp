f8881d8e3f42ef367fcba0275bf25702b051c303
Alexandra <malceva.aleksandra@mail.ru>
2019-05-24 20:23:07 +0300

    "разница введенных значений"

--- main.cpp
+++ main.cpp
@@ -5,5 +5,7 @@ int main(void)
     int a, b, c;
     printf("Enter 2 numbers\n");
     scanf("%d%d", &a, &b);
-    printf("a+b=%d", a+b, a, b);
+    printf("a+b=%d\n", a+b, a, b);
+    printf("a-b=%d", a-b, a, b);
+
 }
